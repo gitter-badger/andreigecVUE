0be3c14de7f1a84d97e7a07a66ebfecbe889958a
Andrei Gec <andreigec@hotmail.com>
2016-02-03 03:52:49 +1100

    wip

---
+++ js/directives.js
@@ -0,0 +1,5 @@
+Vue.component('TitlePreview', {
+template: '#title-preview',
+
+
+})
--- js/store.js
+++ js/store.js
@@ -4,15 +4,37 @@
 
 	'use strict';
 
-	var STORAGE_KEY = 'todos-vuejs';
+	var baseURL = 'https://intense-inferno-4020.firebaseio.com/Backgrounder/Website/';
+// 	var refAuth={};
+// ref.authWithCustomToken("3ntueFfhOYlbibLUAYtygmbBIFu4ROP7dLZnHHFI", function(error, authData) {
+//   if (error) {
+//     console.log("Authentication Failed!", error);
+//   } else {
+//     console.log("Authenticated successfully with payload:", authData);
+//     refAuth=authData;
+//   }
+// });
 
-	exports.todoStorage = {
-		fetch: function () {
-			return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
-		},
-		save: function (todos) {
-			localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-		}
-	};
+	//var TitlesListFB = new Firebase(baseURL + 'Titles/List');
+	//var TitlesListData=[];
+
+	var TitlesPopularFB = new Firebase(baseURL + 'Titles/Popular');
+	
+// 	TitlesListFB.on('value', function (snapshot) {
+// TitlesListData=snapshot;
+// });
+
+	TitlesPopularFB.on('value', function (snapshot) {
+		var item = snapshot.val()
+		console.log('1');
+		console.log(item);
+		item.forEach(function(s){
+app.PopularTitles.push(s);			
+console.log('.');
+		});
+});
+
+
+	
 
 })(window);
