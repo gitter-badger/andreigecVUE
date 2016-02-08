52491fce73d5280563b31cb772443061a7d32cc5
andrei gec <andreigec@hotmail.com>
2016-02-09 10:28:17 +1100

    coverage

--- build/karma.conf.js
+++ build/karma.conf.js
@@ -5,14 +5,26 @@ module.exports = function (config) {
   config.set({
     browsers: ['PhantomJS'],
     frameworks: ['jasmine'],
-    reporters: ['spec'],
+    reporters: ['spec', 'coverage'],
     files: ['../test/unit/index.js'],
     preprocessors: {
-      '../test/unit/index.js': ['webpack']
+      '../test/unit/index.js': ['webpack', 'coverage']
     },
     webpack: webpackConf,
     webpackMiddleware: {
       noInfo: true
+    },
+     coverageReporter: {
+      // specify a common output directory
+      dir: 'reports',
+      reporters: [
+        // reporters not supporting the `file` property
+        { type: 'html', subdir: 'report-html' },
+        { type: 'lcov', subdir: 'report-lcov' },
+        // reporters supporting the `file` property, use `subdir` to directly
+        // output them in the `dir` directory
+        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' }
+      ]
     }
-  })
+})
 }
