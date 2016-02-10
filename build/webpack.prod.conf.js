743c7b9839ebbb19c6a32d1fc9ac8bba689b9778
andrei gec <andreigec@hotmail.com>
2016-02-10 15:26:06 +1100

    test

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,16 +8,12 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 config.output.filename = '[name].[chunkhash].js'
 config.output.chunkFilename = '[id].[chunkhash].js'
 
-// whether to generate source map for production files.
-// disabling this can speed up the build.
-var SOURCE_MAP = true
-
-config.devtool = SOURCE_MAP ? 'source-map' : false
+config.devtool = false
 
 // generate loader string to be used with extract text plugin
 function generateExtractLoaders (loaders) {
   return loaders.map(function (loader) {
-    return loader + '-loader' + (SOURCE_MAP ? '?sourceMap' : '')
+    return loader + '-loader' + '?sourceMap'
   }).join('!')
 }
 
@@ -44,6 +40,10 @@ config.plugins = (config.plugins || []).concat([
   }),
   new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
   new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.SourceMapDevToolPlugin({
+        filename: "[file].map",
+        exclude: ["vendor.bundle.js"]
+      }),
   // extract css into its own file
   new ExtractTextPlugin('[name].[contenthash].css'),
   // generate dist index.html with correct asset hash for caching.
