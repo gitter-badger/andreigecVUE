806abc7d89ed4e28d46e27953868e0b3030908ed
andrei gec <andreigec@hotmail.com>
2016-02-26 17:06:16 +1100

    wip issue #4

---
+++ src/components/topbar-nav.vue
@@ -0,0 +1,91 @@
+<style lang='sass' scoped>
+
+header {
+    &.custom-header {
+        background: rgba(63, 81, 181, 0.9);
+    }
+    position:fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+}
+
+.title {
+    font-size: 2rem;
+    color: white;
+}
+
+.item {
+    font-size: 1.5rem;
+    color: white;
+    text-transform: inherit;
+}
+
+</style>
+
+<template>
+
+<div v-if='sidebar'>
+    <nav class="mdl-navigation">
+        <a class="mdl-navigation__link" href="#">Hello</a>
+        <a class="mdl-navigation__link" href="#">World.</a>
+        <a class="mdl-navigation__link" href="#">How</a>
+        <a class="mdl-navigation__link" href="#">Are</a>
+        <a class="mdl-navigation__link" href="#">You?</a>
+    </nav>
+</div>
+<div v-if='!sidebar'>
+    <nav class="mdl-navigation">
+        <a href='https://s3-ap-southeast-2.amazonaws.com/andreigec/CV/AndreiGecCV.doc' target="_blank">
+            <mdl-button colored v-mdl-ripple-effect>
+                <div class='item'>CV</div>
+            </mdl-button>
+        </a>
+        <a class='mdl-navigation__link' v-link="{ path: '/Titles/Browse/Games', exact: true}">
+            <mdl-button colored v-mdl-ripple-effect>
+                <div class='item'>Games</div>
+            </mdl-button>
+        </a>
+        <a class='mdl-navigation__link' v-link="{ path: '/Titles/Browse/Applications', exact: true}">
+            <mdl-button colored v-mdl-ripple-effect>
+                <div class='item'>Applications</div>
+            </mdl-button>
+        </a>
+
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
+          mdl-textfield--floating-label mdl-textfield--align-right" v-if='showSearch'>
+            <mdl-textfield id="demo-expandable-button" expandable="search"></mdl-textfield>
+        </div>
+    </nav>
+</div>
+
+</template>
+
+<script>
+
+var vmdl = require('vue-mdl')
+var button = vmdl.components['mdl-button']
+var ripple = vmdl.directives['mdl-ripple-effect']
+var textfield = vmdl.components['mdl-textfield']
+module.exports = {
+    props: {
+        showSearch: {
+            type: Boolean,
+            required: true
+        },
+        sidebar: {
+            type: Boolean,
+            required: true
+        }
+    },
+
+    components: {
+        'mdl-button': button,
+        'mdl-textfield': textfield
+    },
+    directives: {
+        'mdl-ripple-effect': ripple
+    }
+}
+
+</script>
--- src/components/topbar.vue
+++ src/components/topbar.vue
@@ -5,7 +5,9 @@ header {
         background: rgba(63, 81, 181, 0.9);
     }
     position:fixed;
-    top:0;
+    top: 0;
+    right: 0;
+    left: 0;
 }
 
 .title {
@@ -23,51 +25,23 @@ header {
 
 <template>
 
-<header class="custom-header mdl-layout__header mdl-layout__header--waterfall">
-    <div class="mdl-layout__header-row">
-        <!-- Title -->
-        <a class='title' v-link="{ path: '/', exact: true}">
-            <mdl-button colored v-mdl-ripple-effect>
-                <div class='title'>TESTRESAS.TES</div>
-            </mdl-button>
-        </a>
-        <div class="mdl-layout-spacer"></div>
-        <nav class="mdl-navigation mdl-layout--large-screen-only">
-            <a href='https://s3-ap-southeast-2.amazonaws.com/andreigec/CV/AndreiGecCV.doc' target="_blank">
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+    <header class="custom-header">
+        <div class="mdl-layout__header-row">
+            <!-- Title -->
+            <a class='title' v-link="{ path: '/', exact: true}">
                 <mdl-button colored v-mdl-ripple-effect>
-                    <div class='item'>CV</div>
+                    <div class='title'>TESTRESAS.TES</div>
                 </mdl-button>
             </a>
-            <a class='mdl-navigation__link' v-link="{ path: '/Titles/Browse/Games', exact: true}">
-                <mdl-button colored v-mdl-ripple-effect>
-                    <div class='item'>Games</div>
-                </mdl-button>
-            </a>
-            <a class='mdl-navigation__link' v-link="{ path: '/Titles/Browse/Applications', exact: true}">
-                <mdl-button colored v-mdl-ripple-effect>
-                    <div class='item'>Applications</div>
-                </mdl-button>
-            </a>
-
-
-            <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
-                  mdl-textfield--floating-label mdl-textfield--align-right">
-                <mdl-textfield id="demo-expandable-button" expandable="search"></mdl-textfield>
-
-            </div>
-
-
-        </nav>
+            <div class="mdl-layout-spacer"></div>
+            <topbar-nav v-bind:show-search='true' v-bind:sidebar='false' ></topbar-nav>
+        </div>
+    </header>
+    <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">Sidebar Menu</span>
+        <topbar-nav v-bind:show-search='false' v-bind:sidebar='true'></topbar-nav>
     </div>
-</header>
-<div class="mdl-layout__drawer">
-    <span class="mdl-layout-title">Sidebar Menu</span>
-    <nav class="mdl-navigation">
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-    </nav>
 </div>
 
 </template>
@@ -78,11 +52,13 @@ var vmdl = require('vue-mdl')
 var button = vmdl.components['mdl-button']
 var ripple = vmdl.directives['mdl-ripple-effect']
 var textfield = vmdl.components['mdl-textfield']
+var topbarnav = require('./topbar-nav')
 module.exports = {
 
     components: {
         'mdl-button': button,
-        'mdl-textfield': textfield
+        'mdl-textfield': textfield,
+        'topbar-nav': topbarnav
     },
     directives: {
         'mdl-ripple-effect': ripple
